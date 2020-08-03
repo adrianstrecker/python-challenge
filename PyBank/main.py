39645b1681cc5dbcfbbcfb55b9d8d39e1559c7a0
Cyragosa <67436390+Cyragosa@users.noreply.github.com>
2020-08-03 18:30:41 -0500

    Updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,14 +2,21 @@
 import os
 import csv
 
-#set file path
+#output results to txt file
+output_file = os.path.join ('Resources', 'budget_results.txt')
+    
+#csv file path
 file_path = os.path.join ('Resources', 'budget_data.csv')
-
 with open(file_path, "r") as csvFile:
-    #Create csv reader
+#Create csv reader
     csv_reader = csv.reader(csvFile, delimiter=',')
     #create variables for sum of month
     data = list(csv_reader)
     sum_month = len(data)-1
 #print total months
 print("Total Months: " + f'{sum_month}')
+
+#set file pate using write mode
+with open(output_file, 'w') as txtFile:
+    #write total months life
+    txtFile.write("Total Months: " + f'{sum_month}')
