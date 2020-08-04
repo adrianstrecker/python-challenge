9d405cd8afe83e59469c0fcdfcae0cc81d02816a
Cyragosa <67436390+Cyragosa@users.noreply.github.com>
2020-08-03 21:51:37 -0500

    Added Report title and removed header from being read

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,20 +3,23 @@ import os
 import csv
 
 #output results to txt file
-output_file = os.path.join ('Resources', 'budget_results.txt')
+output_file = os.path.join ('Analysis', 'budget_results.txt')
     
 #csv file path
 file_path = os.path.join ('Resources', 'budget_data.csv')
 with open(file_path, "r") as csvFile:
 #Create csv reader
     csv_reader = csv.reader(csvFile, delimiter=',')
+    #remove header
+    next(csv_reader)
     #create variables for sum of month
     data = list(csv_reader)
-    sum_month = len(data)-1
+    sum_month = len(data)
 #print total months
 print("Total Months: " + f'{sum_month}')
 
 #set file pate using write mode
 with open(output_file, 'w') as txtFile:
-    #write total months life
-    txtFile.write("Total Months: " + f'{sum_month}')
+    #write total months life]
+    #txtFile.write("Financial Analysis")
+    txtFile.write("Financial Analysis\n\nTotal Months: " + f'{sum_month}')
