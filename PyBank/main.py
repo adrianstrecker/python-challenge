096c08101007dc8bb33e2bbb295f2c1c09028e1b
Cyragosa <67436390+Cyragosa@users.noreply.github.com>
2020-08-04 00:13:14 -0500

    Added coding for total

--- PyBank/main.py
+++ PyBank/main.py
@@ -15,11 +15,22 @@ with open(file_path, "r") as csvFile:
     #create variables for sum of month
     data = list(csv_reader)
     sum_month = len(data)
+    #create variable for total profit/loss
+    #profit_loss_total = 0
+    pltotal = []
+    for row in data:
+        tot_prof = row[1]
+        pltotal.append(tot_prof)
+    pltotal2 = [float(integral) for integral in pltotal]
+    final_pltotal = sum(pltotal2)
 #print total months
-print("Total Months: " + f'{sum_month}')
-
+print("Financial Analysis\n\nTotal Months: " + f'{sum_month}')
+print("Total: " + f'{final_pltotal}'.rstrip('.0'))
+#---------------------------------------------------------------------------
 #set file pate using write mode
 with open(output_file, 'w') as txtFile:
     #write total months life]
     #txtFile.write("Financial Analysis")
-    txtFile.write("Financial Analysis\n\nTotal Months: " + f'{sum_month}')
+    txtFile.write("Financial Analysis\n---------------------------------\nTotal Months: "+ f'{sum_month}'"\nTotal: " + f'{final_pltotal}'.rstrip('.0'))
+    #write total profit/loss
+    #txtFile.write("Total: " + f'{final_pltotal}'.rstrip('.0'))
---
+++ PyPoll/main.py
@@ -0,0 +1,16 @@
+#open file
+#create variables - totalVotes int, candidateList = {candidate: votes}
+#open CSV for read
+#csv_file_object.next()
+#for each row
+#total votes = totalVotes +1
+#if candidateList.index(current row candidate) <1 
+	#then append dict to list and set votes = 0
+#increase vote for candidate + 1
+#exit for loop
+#create temp variables
+#for candidate in list
+	#find one with most votes ( accumulator? )
+	#calc and print percentage
+#print outPut
+	
