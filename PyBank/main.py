09f047a58c753ab01f341212a4f5795bc143952f
Cyragosa <67436390+Cyragosa@users.noreply.github.com>
2020-08-04 17:44:37 -0500

    Finished PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -18,20 +18,20 @@ with open(file_path, "r") as csvFile:
     sum_month = len(data)
     #create variable for total profit/loss
     pltotal = []
+    date = []
     for row in data:
         pltotal.append(float(row[1]))
+        date.append(row[0])
     final_pltotal = sum(pltotal)
     #create variables for monthly change
     monthly_change = []
-    previous_month = 0.00
-    current_month = 0.00
-    date = []
     for i in range(1,len(pltotal)):
         monthly_change.append(pltotal[i] - pltotal[i-1])
         avg_change = sum(monthly_change)/len(monthly_change)
         #create greatest increase
         greatest_increase = max(monthly_change)
-        #greatest_date = str(date[monthly_change.index(max(monthly_change))])
+        greatest_date = str(date[monthly_change.index(max(monthly_change))+1])
+        greatest_decrease_date = str(date[monthly_change.index(min(monthly_change))+1])
         #create greatest decrease
         greatest_decrease = min(monthly_change)
 #print total months
@@ -39,10 +39,10 @@ print("Financial Analysis\n\nTotal Months: " + f'{sum_month}')
 #print total profit/loss
 print("Total: " + f'${final_pltotal}'.rstrip('.0'))
 print("Average Change: " + f'${round(avg_change, 2)}')
-print("Greatest Increase in Profits: " + f'${greatest_increase}'.rstrip('.0'))
-print("Greatest Decrease in Profits: " + f'${greatest_decrease}'.rstrip('.0'))
+print("Greatest Increase in Profits: " + f'{greatest_date}',(f'${greatest_increase}'.rstrip('.0')))
+print("Greatest Decrease in Profits: " + f'{greatest_decrease_date}', (f'${greatest_decrease}'.rstrip('.0')))
 #---------------------------------------------------------------------------
 #set file pate using write mode
 with open(output_file, 'w') as txtFile:
     #output results to .txt file
-    txtFile.write("Financial Analysis\n---------------------------------\nTotal Months: "+ f'{sum_month}'"\nTotal: " + f'${final_pltotal}'.rstrip('.0') + "\nAverage Change: " + f'${round(avg_change, 2)}' + "\nGreatest Increase in Profits: " + f'${greatest_increase}'.rstrip('.0') + "\nGreatest Decrease in Profits: " + f'${greatest_decrease}'.rstrip('.0'))
+    txtFile.write("Financial Analysis\n---------------------------------\nTotal Months: "+ f'{sum_month}'"\nTotal: " + f'${final_pltotal}'.rstrip('.0') + "\nAverage Change: " + f'${round(avg_change, 2)}' + "\nGreatest Increase in Profits: "+ f'{greatest_date}'+ " (" + (f'${greatest_increase}'.rstrip('.0')) + ")" + "\nGreatest Decrease in Profits: "+ f'{greatest_decrease_date}'+ " (" + (f'${greatest_decrease}'.rstrip('.0')) + ")")
