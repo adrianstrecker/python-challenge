ff3b86314edc702e7d42e66a3678e34f1821e032
Cyragosa <67436390+Cyragosa@users.noreply.github.com>
2020-08-05 13:50:14 -0500

    Vote count for each candidate coded

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,24 +9,46 @@ output_file = os.path.join ('Analysis', 'election_results.txt')
 file_path = os.path.join ('Resources', 'election_data.csv')
 with open(file_path, "r") as csvFile:
 #Create csv reader
-    csv_reader = csv.reader(csvFile, delimiter=',')
-    #remove header
-    next(csv_reader)
-    #create variables for sum of month
-    #https://stackoverflow.com/questions/27504056/row-count-in-a-csv-file
-    #data = list(csv_reader)
-    votes = []
-    #date = []
+    csv_reader = csv.DictReader(csvFile, delimiter=',')
+    #Get total votes
+    total_votes = -1
+    for row in open(file_path):
+        total_votes += 1
+    #Get vote count for each candidate
+    #Candidate Khan
+    candidate_k = []
+    #Candidate Correy
+    candidate_c = []
+    #Candidate Li
+    candidate_l = []
+    #Candidate O'Tooley
+    candidate_o = []
     for row in csv_reader:
-        votes.append(row[1])
-        #date.append(row[0])
-    total_votes = len(votes)
-    print("Election Results"+ "\n-------------------------------"+ "\nTotal Votes: " + f'{total_votes}')
+        if row['Candidate'] == 'Khan':
+         candidate_k.append(row)
+         candidate_k_total = len(candidate_k)
+        elif row['Candidate'] == 'Correy':
+         candidate_c.append(row)
+         candidate_c_total = len(candidate_c)
+        elif row['Candidate'] == 'Li':
+         candidate_l.append(row)
+         candidate_l_total = len(candidate_l)
+        else:
+         candidate_o.append(row)
+         candidate_o_total = len(candidate_o)
+        
+print("Election Results"+ "\n-------------------------------"+ "\nTotal Votes: " + f'{total_votes}')
+print("-------------------------------")
+#print Khan total
+print("Khan: " + f'{candidate_k_total}')
+#print Correy total
+print("Correy: " + f'{candidate_c_total}')
+#print Li total
+print("Li: " + f'{candidate_l_total}')
+#print O'Tooley total
+print("O'Tooley: " + f'{candidate_o_total}')
 # candidateList = {candidate: votes}
-#open CSV for read
-#csv_file_object.next()
 #for each row
-#total votes = totalVotes +1
 #if candidateList.index(current row candidate) <1 
 	#then append dict to list and set votes = 0
 #increase vote for candidate + 1
