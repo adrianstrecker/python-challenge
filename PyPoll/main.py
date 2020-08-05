7f41e451fa37248ca2056c4d87da1de11ede484f
Cyragosa <67436390+Cyragosa@users.noreply.github.com>
2020-08-04 20:43:54 -0500

    Total votes coded

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,5 +1,28 @@
-#open file
-#create variables - totalVotes int, candidateList = {candidate: votes}
+#import modules
+import os
+import csv
+
+#output results to txt file
+output_file = os.path.join ('Analysis', 'election_results.txt')
+    
+#set csv file path being read
+file_path = os.path.join ('Resources', 'election_data.csv')
+with open(file_path, "r") as csvFile:
+#Create csv reader
+    csv_reader = csv.reader(csvFile, delimiter=',')
+    #remove header
+    next(csv_reader)
+    #create variables for sum of month
+    #https://stackoverflow.com/questions/27504056/row-count-in-a-csv-file
+    #data = list(csv_reader)
+    votes = []
+    #date = []
+    for row in csv_reader:
+        votes.append(row[1])
+        #date.append(row[0])
+    total_votes = len(votes)
+    print(total_votes)
+# candidateList = {candidate: votes}
 #open CSV for read
 #csv_file_object.next()
 #for each row
@@ -13,4 +36,6 @@
 	#find one with most votes ( accumulator? )
 	#calc and print percentage
 #print outPut
-	
+with open(output_file, 'w') as txtFile:
+    #output results to .txt file
+    txtFile.write("Election Results"+ "\n-------------------------------"+ "\nTotal Votes: " + f'{total_votes}')	
